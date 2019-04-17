29a229d453049ab93c5b9708642320a46965ad08
bnonni <bryanwnonni@gmail.com>
2019-04-17 18:10:37 -0400

    commiting change

--- 17-reverse/reverseP.c
+++ 17-reverse/reverseP.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#define N 30
+#define N 50
 
 int main()
 {
-	int arr[N], *p;
+	int arr[N] = {0}, *p;
  p=&arr[0];
 	char c;
 	printf("Enter a message: ");
@@ -14,10 +14,10 @@ int main()
 	}
 			
 	printf("Reversal is: ");
-
+	p=&arr[N-1];
 	while(p != arr-1){
 		putchar(*p);
-  p--;
+  --p;
  }
 printf("\n");
 return  0;
